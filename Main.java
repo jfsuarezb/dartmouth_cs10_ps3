46654d18e5585dec427169d2e05aa2e0977cc828
Juan Felipe Suarez <juanfelipesuarez@Juans-MacBook-Air.local>
2022-02-10 23:49:04 -0500

    Final push. Everything done

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 	public static void main(String[] args) {
-		String fileName = "hello.txt";
+		String fileName = "WarAndPeace.txt";
 		CompressorFile compressor = new CompressorFile(fileName);
 		compressor.compress();
 		compressor.deCompress();				
